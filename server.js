24040d5d17a71815bb6406f8512ebf1fe3dbd390
Petermuturi <pmuturi65@gmail.com>
2016-11-28 11:35:47 +0300

    res.send all data fetched

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-
+const Crawler = require('simplecrawler');
 
  app.get('/', (req, res)=> {
-
+var x = [], y = 0;
  	var crawler = new Crawler("http://petermuturi.xyz/");
 
 	crawler.on("crawlstart", function() {
@@ -12,19 +12,20 @@ var app = express();
 
 	crawler.on("fetchstart", function(queueItem) {
 	    console.log ("fetchStart", queueItem);
+      x.push(queueItem)
 	});
 
 	crawler.on("fetchcomplete", function(queueItem) {
 	    console.log ("fetchcomplete", queueItem);
+
 	});
 
 	crawler.on("complete", function() {
 	    console.log ("Finished!");
-	    res.send('complete')
+      res.send(x)
 	});
 	crawler.maxDepth = 2;
 	crawler.start();
-
 
 
 
